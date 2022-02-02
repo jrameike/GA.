e30dbeee9bc74e05e13e28c96dc44ae6ce3de310
jrameike <95244728+jrameike@users.noreply.github.com>
2022-02-02 16:09:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+var nav = document.getElementById("nav");
+var links = nav.getElementsByClassName("link");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
